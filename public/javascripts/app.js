0553eba4d88522f24caa40fd5fc5693c5e1be96d
Justin Knutson <jknutson@alumni.princeton.edu>
2014-02-14 16:43:27 -0500

    Added ability to import lists from mailchimp

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -28,16 +28,19 @@ app.controller('MailchimpController', function($scope, $http) {
 });
 
 
-app.controller('ExportsController', function($scope, $http, $q) {
+app.controller('ExportsController', function($scope, $http) {
   $scope.lists = [];
-  $scope.newList = {
-    id : '',
-    name : ''
-  };
-
+  
   $scope.getLists = function() {
-    $http.get('/exports/lists.json').then(function(data) {
+    $http.get('/exports/lists.json').success(function(data) {
       $scope.lists = data.lists;
+    }).then(function(data) {
+      var newList = {id : '', name : ''};
+      for(var i = 0; i < $scope.lists.length; i++) {
+        newList.id = $scope.lists[i].id;
+        newList.name = $scope.lists[i].name;
+        $http.post('/lists.json', newList);
+      }
     });
   };
 });
