3f0680a630ff592908f8db72e3533250e98fd529
Justin Knutson <jknutson@alumni.princeton.edu>
2014-02-20 11:05:51 -0500

    Removed test ping MC Server

--- routes/routes.js
+++ routes/routes.js
@@ -114,18 +114,8 @@ module.exports = function(app, passport, api, exportApi, crm) {
 	 * MAILCHIMP
 	 ***************************************/
 
+  // Mailchimp test page
 	app.get('/mailchimp', isLoggedIn, function(req, res) {
-		mc.helper.ping(function(data) {
-			res.render('mailchimp', { title : 'Mailchimp' });
-		}, function(err) {
-			console.log(err);
-			if(err.name == 'Invalid_ApiKey') {
-				res.locals.error_flash = "Invalid API key. Set it in app.js";
-			} else if (error.error) {
-				res.locals.error_flash = error.code + ": " + error.error;
-			} else {
-				res.locals.error_flash = "An unknown error occurred";
-			}
 			res.render('mailchimp', { title: 'Mailchimp' });
 		});
 	});
