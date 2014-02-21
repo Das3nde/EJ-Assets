cdd73b212a263c8feb947ac5574faae532d9641f
Justin Knutson <jknutson@alumni.princeton.edu>
2014-02-21 10:38:30 -0500

    Added test abstract function calls to OnePage config

--- config/onepage.js
+++ config/onepage.js
@@ -11,7 +11,46 @@ function OnePageCRM (uid, key) {
 
 module.exports = OnePageCRM;
 
-OnePageCRM.prototype.createContact = function(firstname, lastname, zip_code, phone, email) {
+OnePageCRM.prototype.execute = function(path, method, params) {
+  var self = this;
+
+  var timestamp = parseInt((Date.now()/1000)).toString();
+  var uri = 'https://app.onepagecrm.com/api/' + path;
+  var uri_hash = crypto.createHash('sha1').update(uri).digest('hex');
+
+  var hash_string = this.uid + '.' + timestamp + '.' + method + '.' + hash_uri;
+
+  if(method == 'POST' || method == 'PUT') {
+    var qs_params = qs.stringify(params);
+    var params_hash = crypto.createHash('sha1').update(qs_params).digest('hex');
+    hash_string += params_hash;
+  }
+
+  buffer = new Buffer(this.key, 'base64');
+
+  auth = crypto.createHmac('sha256', buffer).update(hash_string).digest('hex');
+
+  request({
+    method : method,
+    uri : uri,
+    headers : {
+      'X-OnePageCRM-UID' : this.uid,
+      'X-OnePageCRM-TS' : timestamp,
+      'X-OnePageCRM-Auth' : auth
+    },
+    body : params
+  }, function(error, response, body) {
+    res = JSON.parse(body);
+    console.log(res);
+  });
+}
+
+OnePageCRM.prototype.createContact = function(params) {
+  this.execute('contacts.json', 'POST', params);
+}
+
+
+OnePageCRM.prototype.oldCreateContact = function(firstname, lastname, zip_code, phone, email) {
   var ts = parseInt((Date.now()/1000)).toString(),
       uri = 'https://app.onepagecrm.com/api/contacts.json',
       hash_uri = crypto.createHash('sha1').update(uri).digest('hex'),
