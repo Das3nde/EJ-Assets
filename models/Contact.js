7e0fa264619f368f2a9f5a976c4c7c6d99b2eacf
Justin Knutson <jknutson@alumni.princeton.edu>
2014-02-24 14:16:15 -0500

    Added generic Contact model

---
+++ models/Contact.js
@@ -0,0 +1,27 @@
+var Mongoose = require('mongoose');
+
+var ContactSchema = Mongoose.Schema({
+  email : String,
+  first_name : String,
+  last_name : String,
+  phone : String
+});
+
+module.exports = Mongoose.model('Contact', ContactSchema);
+
+
+/**************************************
+ * For dev reference later
+ **************************************/
+
+/*
+UserSchema.methods.generateHash = function(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+UserSchema.methods.validPassword = function(password) {
+  return bcrypt.compareSync(password, this.password);
+};
+
+module.exports = Mongoose.model('User', UserSchema);
+*/
