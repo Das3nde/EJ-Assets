f4df0fbf7f1e814d850b65254d912d1cff7a6f1a
Justin Knutson <jknutson@alumni.princeton.edu>
2014-03-24 16:18:15 -0400

    Added files

---
+++ routes/pages.js
@@ -0,0 +1,131 @@
+/***********************************
+ * Author: Justin Knutson
+ * Updated: 28 February 2014
+ * Purpose: Route all GET call to pages
+ ***********************************/
+
+/***********************************
+ * HOMEPAGE/INDEX
+ ***********************************/
+
+exports.index = function(req, res) {
+  return function(req, res) {
+    res.render('home', {title : 'Eleven James'});
+  };
+};
+
+/***********************************
+ * LOGIN PAGE
+ ***********************************/
+
+exports.login = function() {
+  return function(req, res) {
+    res.render('login');
+  };
+};
+
+/***********************************
+ * SIGN-UP PAGE
+ ***********************************/
+exports.signup = function() {
+  return function(req, res) {
+    res.render('signup');
+  };
+};
+
+/***********************************
+ * WATCH DIRECTORY PAGE
+ ***********************************/
+
+exports.watches = function(Watch) {
+  return function(req, res) {
+    Watch.find({}, function(error, watches) {
+      res.render('directory', {title : 'EJ Watches', watches : watches });
+    });
+  };
+};
+
+/***********************************
+ * ADD WATCH FORM PAGE
+ ***********************************/
+
+exports.addWatch = function() {
+  return function(req, res) {
+    res.render('add_watch');
+  };
+};
+
+/***********************************
+ * VIEW INDIVIDUAL WATCH PAGE
+ ***********************************/
+
+exports.viewWatch = function(Watch) {
+  return function(req, res) {
+    Watch.find({_id : req.params.id}, function(error, watch) {
+      if(error) {
+        console.log("Can't find your fucking watch");
+      } else {
+        res.render('watch', {watch : watch[0]});
+      }
+    });
+  };
+};
+
+/***********************************
+ * GENERATE EMAIL TEMPLATE
+ ***********************************/
+
+exports.generateTemplate = function(Watch) {
+  return function(req, res) {
+    Watch.find({_id : req.params.id}, function(error, watch) {
+      if(error) {
+        console.log("Can't find your fucking watch");
+      } else {
+        res.render('template', { title: 'Template', watch : watch[0]});
+      }
+    });
+  };
+};
+
+/***********************************
+ * DUMP WATCH INFO
+ ***********************************/
+
+exports.watchInfo = function(Watch) {
+  return function(req, res) {
+    Watch.find({}, function(error, watches) {
+      res.render('watch-info', {title: 'Watch Info', watches : watches});
+    });
+  };
+};
+
+/***********************************
+ * INDIVIDUAL WATCH INFO PAGE
+ ***********************************/
+
+exports.watchPage = function(Watch) {
+  return function(req, res) {
+    Watch.find({_id : req.params.id}, function(error, watches) {
+      res.render('watch-frame', {title: watches[0].brand});
+    });
+  };
+};
+
+/***********************************
+ * GET MEMBERS
+ ***********************************/
+
+exports.getMembers = function() {
+  return function(req, res) {
+    res.render('members', {title: 'Member Info'});
+  };
+};
+/***********************************
+ * MAILCHIMP HOME PAGE
+ ***********************************/
+
+exports.mailchimp = function() {
+  return function(req, res) {
+    res.render('mailchimp', {title : 'Mailchimp' });
+  };
+};
