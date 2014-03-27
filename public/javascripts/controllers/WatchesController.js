65d7797619821b16536b6d9ff5bf3cab66fa3bd5
Justin Knutson <jknutson@alumni.princeton.edu>
2014-03-27 12:10:19 -0400

    Renamed controller

---
+++ public/javascripts/controllers/WatchesController.js
@@ -0,0 +1,53 @@
+/***********************************
+ * Author: Justin Knutson
+ * Updated: 28 February 2014
+ * Purpose: Control Adding and Updating Watches
+ ***********************************/
+
+function WatchesController($scope, $http) {
+  $scope.watches = [];
+
+  $scope.defaultForm = {
+    brand : '',
+    family : '',
+    model : '',
+    reference : '',
+    serial : '',
+    img : '',
+    instructions : [],
+    description_long : '',
+    description_short : '',
+    trivia : ''
+  };
+
+  $scope.defaultInstructions = [{id : '1'}];
+
+  $scope.watch = angular.copy($scope.defaultForm);
+  $scope.instructions = angular.copy($scope.defaultInstructions);
+
+  $scope.addWatch = function() {
+    $scope.watch.instructions = $scope.instructions;
+    $http.post('/watches.json', $scope.watch).success(function(data) {
+      alert("Successfully added watch!");
+    });
+    $scope.watch = angular.copy($scope.defaultForm);
+    $scope.instructions = angular.copy($scope.defaultInstructions);
+  };
+
+  $scope.addInstruction = function() {
+    $scope.instructions.push({id : $scope.instructions.length+1});
+  };
+
+  $scope.removeWatch = function(watch) {
+    $http.delete('/watches/' + watch._id + '.json').success(function(data) {
+      console.log(data);
+    });
+    $scope.getWatches();
+  };
+
+  $scope.getWatches = function() {
+    $http.get('/watches.json').success(function(data) {
+      $scope.watches = data.watches;
+    });
+  };
+}
