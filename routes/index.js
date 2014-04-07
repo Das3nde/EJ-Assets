7859e491c3e66b1659e7f9702a2b33b167d254bc
Justin Knutson <jknutson@alumni.princeton.edu>
2014-04-07 11:52:30 -0400

    Removed OnePage

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,6 @@ var ZohoCRM = require('../config/zoho.js');
 var zoho = new ZohoCRM('d11a44a5746e17b1c3fcfadf816e619f');
 // Make these keys databased
 
-var OnePageCRM = require('../config/onepage.js');
-var onepage = new OnePageCRM('525da050eb8997663500001e', 'xSWc1f4oYarbhXUtBzRAXx8RH1Iv6zcNRmVefPjuf/U=');
-// Make these keys databased
-
 module.exports = function(passport) {
 
   // Non-CRUD Pages
@@ -17,7 +13,6 @@ module.exports = function(passport) {
 
   // Webhooks
   require('./webhooks/zoho')(zoho);
-  require('./webhooks/onepage')(onepage);
 
   // Watch Ops
   require('./watches')(passport);
