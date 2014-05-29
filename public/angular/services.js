d82170199c16ae66402ce3464f921eaba9da807f
Justin Knutson <jknutson@alumni.princeton.edu>
2014-05-29 15:45:00 -0400

    Removed spinner that only works for ajax

--- public/angular/services.js
+++ public/angular/services.js
@@ -2,32 +2,6 @@
 
 var services = angular.module('services', ['ngResource']);
 
-services.config(function($httpProvider) {
-  $httpProvider.responseInterceptors.push('imageRequestInterceptor');
-
-  var spinnerFunction = function spinnerFunction(data, headersGetter) {
-    $("#spinner").show();
-    $('.watch').hide();
-    return data;
-  };
-
-  $httpProvider.defaults.transformRequest.push(spinnerFunction);
-});
-
-services.factory('imageRequestInterceptor', function($q, $window) {
-  return function(promise) {
-    return promise.then(function (response) {
-      $("#spinner").hide();
-      $(".watch").show();
-      return response;
-    }, function(response) {
-      $("#spinner").hide();
-      $(".watch").show();
-      return $q.reject(response);
-    });
-  };
-});
-
 services.factory('Watches', ['$resource', function($resource) {
   return $resource('/watches.json');
 }]);
