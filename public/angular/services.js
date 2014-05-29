665e8f8b04e04c2760766c370809a8c6a0d7002e
Justin Knutson <jknutson@alumni.princeton.edu>
2014-05-29 15:27:22 -0400

    Attempting to add ajax interceptor as service

--- public/angular/services.js
+++ public/angular/services.js
@@ -2,6 +2,29 @@
 
 var services = angular.module('services', ['ngResource']);
 
+services.config(function($httpProvider) {
+  $httpProvider.responseInterceptors.push('imageRequestInterceptor');
+
+  var spinnerFunction = function spinnerFunction(data, headersGetter) {
+    $("#spinner").show();
+    return data;
+  };
+
+  $httpProvider.defaults.transformRequest.push(spinnerFunction);
+});
+
+services.factory('imageRequestInterceptor', function($q, $window) {
+  return function(promise) {
+    return promise.then(function (response) {
+      $("#spinner").hide();
+      return response;
+    }, function(response) {
+      $("#spinner").hide();
+      return $q.reject(response);
+    });
+  };
+});
+
 services.factory('Watches', ['$resource', function($resource) {
   return $resource('/watches.json');
 }]);
