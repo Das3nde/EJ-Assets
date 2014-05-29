971e86fe7e5fdd2018941b720ef3380750aa94d8
Justin Knutson <jknutson@alumni.princeton.edu>
2014-05-29 16:25:44 -0400

    Added spinnner.gif

---
+++ public/angular/directives.js
@@ -0,0 +1,38 @@
+'use strict'
+
+var directives = angular.module('directives', []);
+
+directives.directive("mySrc", ['$log', function($log) {
+  return {
+    link: function(scope, element, attrs) {
+      var img, loadImage;
+      img = null;
+
+      loadImage = function() {
+
+        element.hide();
+
+        //element[0].src = "/images/spinner.gif";
+
+        img = new Image();
+        $log.log('Attrs is ' + attrs.mySrc);
+        img.src = attrs.mySrc;
+
+        img.onload = function() {
+          $log.log('Img loaded with src ' + attrs.mySrc);
+          element[0].src = attrs.mySrc;
+          element.show();
+        };
+      };
+          
+
+      scope.$watch((function() {
+        return attrs.mySrc;
+      }), function(newVal, oldVal) {
+        if(oldVal !== newVal) {
+          loadImage();
+        }
+      });
+    }
+  };
+}]);
